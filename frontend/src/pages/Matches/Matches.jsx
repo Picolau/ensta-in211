10f3da1dea6d6a329ae7e712351e68e63ce53b78
Picolau <lgpicoli@hotmail.com>
2023-05-09 14:31:38 +0200

    matches front integrated without movies

--- frontend/src/pages/Matches/Matches.jsx
+++ frontend/src/pages/Matches/Matches.jsx
@@ -5,6 +5,7 @@ import './Matches.css';
 import AuthContext from '../../hooks/useSession';
 
 function Matches() {
+  const [userMatches, setUserMatches] = useState([]);
   const { loggedUser } = useContext(AuthContext);
   const [counter, setCounter] = useState(0);
 
@@ -12,15 +13,34 @@ function Matches() {
     axios
       .get(`${URL_API}/userMovie/matches?user_id=${loggedUser.id}`)
       .then((results) => {
-        console.log(results.data);
+        setUserMatches(results.data);
       });
   }, []);
 
   return (
-    <div className="Counter-container">
-      <h1>This is a counter example</h1>
-      <div>Counter value : {counter}</div>
-      <button onClick={() => setCounter(counter + 1)}>Increment counter</button>
+    <div className="Matches-container">
+      <h1>Voici your best matches!</h1>
+      <div className="Matches-user">
+        {userMatches.map((userMatch) => {
+          return (
+            <div className="Matches-user-info">
+              <div className="user">
+                <a href={`/home?search=$${userMatch.username}`}>
+                  {userMatch.username}
+                </a>
+              </div>
+              <div className="score">
+                <strong>{Math.round(userMatch.score * 10000) / 100}%</strong> of
+                match
+              </div>
+              <div className="length">
+                <strong>{userMatch.sameMoviesRated.length}</strong> movies that
+                both of you rated!
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
